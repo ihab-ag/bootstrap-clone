643f1e0eeb6491aab27171d28f26fa36b9b3f688
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-12 21:07:08 +0300

    added toggle display function for menu

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ window.onload=()=>{
     // elements
     const btnMenu = document.getElementById('menu-btn');
     const menu = document.getElementById('menu');
+
+    // functions
+    // toggle displaymenu
+    function toggleMenu(){
+        let display = menu.style.display;
+        display=="none"?display="flex":display="none";
+    }
 }

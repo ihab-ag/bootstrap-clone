ce120fbc8a623d37e22cc7c81559d4e7f4eacc93
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-13 23:43:23 +0300

    get results

--- apis/contacts.php
+++ apis/contacts.php
@@ -5,5 +5,5 @@ include("db_connection.php");
 $query= $mysqli->prepare("SELECT * FROM contact_us");
 $query->execute();
 // get results from query
-$array=$query->get_result()
+$array=$query->get_result();
 ?>

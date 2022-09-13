62b4945b64459707fb0cd67f1b793a32a38eb6de
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-13 23:44:05 +0300

    fill response

--- apis/contacts.php
+++ apis/contacts.php
@@ -6,4 +6,9 @@ $query= $mysqli->prepare("SELECT * FROM contact_us");
 $query->execute();
 // get results from query
 $array=$query->get_result();
+// fill response
+$response = [];
+while($a = $array->fetch_assoc()){
+    $response[] = $a;
+}
 ?>

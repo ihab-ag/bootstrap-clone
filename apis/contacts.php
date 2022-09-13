464bcfc454ab883ecf3f54b107b6369c3b5e6f6e
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-13 23:42:24 +0300

    fixed pointers

--- apis/contacts.php
+++ apis/contacts.php
@@ -2,6 +2,8 @@
 // connect to db
 include("db_connection.php");
 // select * query and execute
-$query= $mysqli=>prepare("SELECT * FROM contact_us");
-$query=>execute();
+$query= $mysqli->prepare("SELECT * FROM contact_us");
+$query->execute();
+// get results from query
+$array=$query->get_result()
 ?>

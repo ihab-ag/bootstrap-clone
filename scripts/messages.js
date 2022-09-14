88ab81a3af4fa01e969ba16486deeed97f9993d4
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-14 11:22:54 +0300

    function to get messages from db

--- scripts/messages.js
+++ scripts/messages.js
@@ -1,4 +1,14 @@
 window.onload=()=>{
     // declare elements
     const messages=document.getElementById('messages');
+
+    getMessages();
+    // functions
+    // get data
+    async function getMessages(){
+        await fetch('apis/contacts.php')
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+      
+    }
 }

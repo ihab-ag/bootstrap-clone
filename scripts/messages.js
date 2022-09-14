394206ee8bf76131684060606bfb02135b8b097c
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-14 11:32:42 +0300

    function to display message in container

--- scripts/messages.js
+++ scripts/messages.js
@@ -8,7 +8,20 @@ window.onload=()=>{
     async function getMessages(){
         await fetch('apis/contacts.php')
         .then((response) => response.json())
-        .then((data) => console.log(data));
-      
+        .then((data) => displayMsg(data));
+    }
+    // display message
+    function displayMsg(arr){
+        // create elements
+        let message = arr[0].message;
+        let div = document.createElement('div');
+        let text = document.createElement('p');
+        // assign values
+        div.classList="message container";
+        text.innerText= message;
+        // append childs
+        div.appendChild(text);
+        messages.appendChild(div);
+
     }
 }
